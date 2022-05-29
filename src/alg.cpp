9b97b10d0525e68a7c107f9b12f25cffaf0a23b8
GabovaMasha <94718616+GabovaMasha@users.noreply.github.com>
2022-05-30 00:02:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,31 @@
 // Copyright 2021 NNTU-CS
+#include  <string>
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
 
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> wordTree;
+std::ifstream file(filename);
+if (!file) {
+std::cout << "File error!" << std::endl;
+return  wordTree;
+}
+std::string word = "";
+while(!file.eof()){
+char simv = file.get();
+if (simv >= 65 && simv <= 90) {
+word += tolower(simv);
+} else if (simv >= 97 && simv <= 122) {
+word += simv;
+} else {
+wordTree.add(word);
+word = "";
+}
+}
+file.close();
+return wordTree; 
 }
